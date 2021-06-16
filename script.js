7fdf03b9d5b668f507d3ac22b9b792051c794395
unknown <bnsviv@gmail.com>
2021-06-16 10:50:31 -0300

    second commit

---
+++ script.js
@@ -0,0 +1,25 @@
+Strange instanceof
+importance: 5
+In the code below, why does 
+instanceof return true? We can easily see that a is not created by B().
+
+function A() {}
+function B() {}
+
+A.prototype = B.prototype = {};
+
+let a = new A();
+
+alert( a instanceof B ); // true
+
+## attempt at solution
+
+Yeah, looks strange indeed.
+
+But instanceof does not care about the function, 
+but rather about its prototype, that it matches against the prototype chain.
+
+And here a.__proto__ == B.prototype, so instanceof returns true.
+
+So, by the logic of instanceof, the prototype actually defines 
+the type, not the constructor function.
