dbdee2499659df88f803df9dfe6816d5cd0ae34c
unknown <bnsviv@gmail.com>
2021-06-21 17:55:19 -0300

    second commit

--- script.js
+++ script.js
@@ -23,3 +23,5 @@ And here a.__proto__ == B.prototype, so instanceof returns true.
 
 So, by the logic of instanceof, the prototype actually defines 
 the type, not the constructor function.
+
+// instanceof only cares about the prototype chain, not the function.
